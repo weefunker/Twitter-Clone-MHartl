065ae4e36523886af14a432fc95725318ceae0e0
Alex Overend <weefunker@yahoo.co.uk>
2019-02-19 13:05:20 +0000

    Finish User Sign Up Chapter 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,29 @@ class UsersController < ApplicationController
 
   def show 
     @user = User.find(params[:id])
+    
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+    redirect_to @user
+    flash[:success] = "Welcome to the Sample App!"
+  else 
+    render :new 
   end
   
-  def new
   end
+
+  private 
+
+  def user_params 
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+
 end
