d9ec5c9133670e2b112bf1a84f8fc48446f7dfcb
Alex Overend <weefunker@yahoo.co.uk>
2019-03-06 03:15:37 +0000

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,8 +7,20 @@ module SessionsHelper
 
   # Returns the current logged-in user (if any).
   def current_user
+    # Check for not nil user_id [session]
     if session[:user_id]
+      # If user does not exist create one by the session if they have still got session cookie
       @current_user ||= User.find_by(id: session[:user_id])
+      # Elseif the user has a permanent signed cookie 
+    elsif cookies.signed[:user_id]
+      # Find the user id of the cookie
+      user = User.find_by(id: cookies.signed[:user_id])
+      # In either case being true proceed to log_in the user and set the current user
+      # Confusion arrises in this line, how come our own authenticated? method works 
+      if user && user.authenticated?(cookies.signed[:user_id])
+        log_in user 
+        @current_user = user 
+      end
     end
   end
 
@@ -22,4 +34,12 @@ module SessionsHelper
     @current_user = nil 
   end
 
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id 
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+    attr_accessor :remember_token
+
     before_save {self.email = email.downcase}
     validates :name, presence: true, length: {minimum:3, maximum:50}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -6,4 +9,29 @@ class User < ApplicationRecord
                 format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_senseitive: false}
     has_secure_password 
     validates :password, presence: true, length: {minimum:8, maximum:44}
+
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                      BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+      end
+
+
+
+     def User.new_token
+        SecureRandom.urlsafe_base64
+     end
+
+      def remember 
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+      end
+
+
+      def authenticated?(remember_token)
+        return false if remember_digest.nil? 
+        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+      end
+
+      
 end 
