d358e9c8ef5d9010829ae71fb13214613edde4f7
Alex Overend <weefunker@yahoo.co.uk>
2019-03-12 01:56:18 +0000

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'password_resets/controller'
+
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :edit, :create, :update]
   get 'users/new'
 
   root 'static_pages#home'
